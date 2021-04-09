4f224ca30e53c1f88a8adbbb18f5405b4f6fb816
Feriel <fnabet@ipconnex.com>
2021-04-09 00:52:46 -0400

    update

--- server/index.js
+++ server/index.js
@@ -44,16 +44,16 @@ app.use("/api/notification", notification);
 
 app.use(error);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 const port = process.env.PORT || 4000;
 
